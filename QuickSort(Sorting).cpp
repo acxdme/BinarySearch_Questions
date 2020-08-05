bed166a3dbcfa0124013ca881bbbec07803e1abf
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-08-05 15:52:23 +0530

    Update QuickSort(Sorting).cpp

--- QuickSort(Sorting).cpp
+++ QuickSort(Sorting).cpp
@@ -1,6 +1,11 @@
 /*
 Time complexity  Analysis : best-O(nlogn)  Average-O(nlogn)  Worst-O(n^2)
+ The worst case occurs when the partition process always picks greatest or smallest element as pivot. 
+ If we consider above partition strategy where last element is always picked as pivot,
+ the worst case would occur when the array is already sorted in increasing or decreasing order
 Space complexity : O (N)
+
+The default implementation is not stable. However any sorting algorithm can be made stable by considering indexes as comparison parameter.
 */
 
 int partition (int arr[], int low, int high)
